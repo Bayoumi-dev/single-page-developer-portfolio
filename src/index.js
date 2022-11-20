eaeb91a8bc41f0a078dd18a98faf0267c7deed95
Ahmed Bayoumi <bayoumi.dev@gmail.com>
2022-11-20 11:43:24 +0200

    build the hero section

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/css/bootstrap-grid.css'
+import 'bootstrap/dist/css/bootstrap-utilities.css'
 import './styles/index.css'
 import app from './js/app'
 app()
