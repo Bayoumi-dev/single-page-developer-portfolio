721cd595b8d414dea6a7306020572401bfb50872
Ahmed Bayoumi <bayoumi.dev@gmail.com>
2022-11-21 15:32:16 +0200

    build the projects section

--- src/index.js
+++ src/index.js
@@ -2,11 +2,4 @@ import 'bootstrap/dist/css/bootstrap-grid.css'
 import 'bootstrap/dist/css/bootstrap-utilities.css'
 import './styles/index.css'
 
-// images
-import './assets/images/icon-github.svg'
-import './assets/images/icon-frontend-mentor.svg'
-import './assets/images/icon-linkedin.svg'
-import './assets/images/icon-twitter.svg'
-import './assets/images/image-profile-mobile.webp'
-import './assets/images/image-profile-tablet.webp'
-import './assets/images/image-profile-desktop.webp'
+
