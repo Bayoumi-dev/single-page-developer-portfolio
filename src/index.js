60f0cf04b4aff1f6bd49f126d9d177edcd85ecaf
Ahmed Bayoumi <bayoumi.dev@gmail.com>
2022-11-21 12:35:52 +0200

    build the skills section

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import 'bootstrap/dist/css/bootstrap-grid.css'
 import 'bootstrap/dist/css/bootstrap-utilities.css'
 import './styles/index.css'
-import app from './js/app'
-app()
+
+// images
+import './assets/images/icon-github.svg'
+import './assets/images/icon-frontend-mentor.svg'
+import './assets/images/icon-linkedin.svg'
+import './assets/images/icon-twitter.svg'
+import './assets/images/image-profile-mobile.webp'
+import './assets/images/image-profile-tablet.webp'
+import './assets/images/image-profile-desktop.webp'
