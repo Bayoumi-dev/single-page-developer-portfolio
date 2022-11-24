11fbb34fc827e3029f5a9813523d556e577e7dbf
Ahmed Bayoumi <bayoumi.dev@gmail.com>
2022-11-24 21:08:12 +0200

    add animation

--- src/index.js
+++ src/index.js
@@ -1,14 +1,5 @@
 import 'bootstrap/dist/css/bootstrap-grid.css'
 import 'bootstrap/dist/css/bootstrap-utilities.css'
 import './styles/index.css'
-
-// images
-import './assets/images/image-profile-mobile.webp'
-import './assets/images/image-profile-tablet.webp'
-import './assets/images/image-profile-desktop.webp'
-import './assets/images/thumbnail-project-1-large.webp'
-import './assets/images/thumbnail-project-2-large.webp'
-import './assets/images/thumbnail-project-3-large.webp'
-import './assets/images/thumbnail-project-4-large.webp'
-import './assets/images/thumbnail-project-5-large.webp'
-import './assets/images/thumbnail-project-6-large.webp'
+import app from './js/app'
+app()
---
+++ src/js/app.js
@@ -0,0 +1,60 @@
+// Global Variable
+const loadingIntro = document.body.querySelector('.intro-section')
+
+const app = () => {
+   // Intro Section
+   setTimeout(() => {
+      loadingIntro.remove()
+      document.body.style.overflow = 'auto'
+   }, 5100)
+
+   /**
+    * Animation On Scroll (with IntersectionObserver)
+    * @description Start the animation when the element is on the viewport
+    */
+   const animations = document.body.querySelectorAll('.animate'),
+      animateOptions = {
+         threshold: 0.3,
+      },
+      animateOnScrolling = new IntersectionObserver(elements => {
+         elements.forEach(element => {
+            if (
+               element.isIntersecting &&
+               !element.target.classList.contains('animated')
+            ) {
+               let duration =
+                     getStyle(element.target, 'animation-duration').replace('s','') * 1000,
+                  animateTimeout = duration + 100
+               startAnimate(element.target, animateTimeout)
+            }
+         })
+      }, animateOptions)
+   // Execute the animateOnScrolling(observer)
+   animations.forEach(animate => {
+      animateOnScrolling.observe(animate)
+   })
+
+   /**
+    * @description get element style
+    * @param {HTMLElement} element - The element to get style for
+    * @param {string} property - The property to get style for
+    * @returns {string} - The style of the element for the property given, empty string if
+    */
+   function getStyle(element, property) {
+      return getComputedStyle(element).getPropertyValue(property)
+   }
+
+   /**
+    * @description Start the animations
+    * @param {HTMLElement} element - The element to animate
+    * @param {Number} timeout - The timeout to wait before starting the animation
+    */
+   const startAnimate = (element, animateTimeout) => {
+      // Add the animated class to the elements that are in the viewport
+      element.classList.add('animated')
+      // Remove the animated class to the elements that are animated after the timeout
+      setTimeout(_ => element.classList.remove('animate'), animateTimeout)
+   }
+}
+
+export default app
